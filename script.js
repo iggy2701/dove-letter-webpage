6a47fd65e57724fc3dbaf317382979026864721c
iggy2701 <Ignatius2701@gmail.com>
2025-10-04 14:26:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const letter = document.getElementById('letter');
+
+letter.addEventListener('click', () => {
+    const canvaLink = "https://www.canva.com/design/DAG0zWDLg0c/pMsOClaFLvpBEZC979ihkQ/edit?utm_content=DAG0zWDLg0c&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton";
+    window.open(canvaLink, "_blank");
+});
